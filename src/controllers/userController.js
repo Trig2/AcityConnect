2316e9421b722a4682d0139a278e1a480d12d2e2
Albert Amuzu <amuzualbert6@gmail.com>
2026-04-29 01:20:27 +0000

    Replace bcrypt with bcryptjs

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,5 +1,5 @@
 import pool from '../db/connection.js';
-import bcrypt from 'bcrypt';
+import bcrypt from 'bcryptjs';
 import jwt from 'jsonwebtoken';
 
 export const register = async (req, res) => {
